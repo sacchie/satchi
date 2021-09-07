230264ce336251717d518b7a0699bfe7241386d5
Kaiichiro Ota <kaiichiro-ota@cybozu.co.jp>
2021-09-07 19:19:05 +0900

    returns notifications as json

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -13,7 +13,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("io.javalin:javalin:3.13.10")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.+")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
     implementation("org.slf4j:slf4j-simple:1.7.30")
 }
 
--- main/src/main/kotlin/main.kt
+++ main/src/main/kotlin/main.kt
@@ -1,6 +1,50 @@
 package main
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.javalin.Javalin
+import java.time.OffsetDateTime
+
 fun main() {
+    val app = Javalin.create().start(8037) /* 37 = "サッチ" */
     val credential = main.github.Credential.load("./.credential")
-    println(main.github.notifications(credential));
+
+    val mapper = jacksonObjectMapper()
+    mapper.registerModule(JavaTimeModule())
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
+    app.get("/notifications") { ctx ->
+        run {
+            val notifications = main.github.notifications(credential).map {
+                Notification(
+                    it.updated_at,
+                    Source(it.repository.name, it.repository.html_url, it.repository.owner.avatar_url),
+                    "GitHub notification",
+                    "${it.subject.title} ${it.subject.url.orEmpty()}",
+                    it.reason in listOf("mention", "team_mention")
+                )
+            }
+            ctx.result(mapper.writeValueAsString(notifications))
+        }
+    }
 }
+
+/**
+ * 通知の発生源(source)から自分に通知(notification)が届くというモデル
+ * 現時点ではsourceは客体・名詞であるとする
+ */
+data class Notification(
+    val timestamp: OffsetDateTime,
+    val source: Source,
+    val title: String,
+    val message: String,
+    val mentioned: Boolean,
+)
+
+data class Source(
+    val name: String,
+    val url: String,
+    val iconUrl: String?
+)
