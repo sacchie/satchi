77a87017004f9583a1b040f973be1490a13fe4fb
hmaeta <hmaeta21+git@gmail.com>
2021-11-09 18:02:10 +0900

    Refactor service

--- main/src/main/kotlin/State.kt
+++ main/src/main/kotlin/State.kt
@@ -9,12 +9,13 @@ class State(
     var notificationList: main.notificationlist.State,
     var filter: main.filter.State,
     var desktopNotification: main.desktopnotification.State,
-    val sendViewModel: (
+    val onChange: (
         notificationListState: main.notificationlist.State,
         filterState: main.filter.State,
     ) -> Unit,
-    val sendDesktopNotification: (
-        notifications: List<Notification>
+    val onChangeDesktop: ( //todo: rename
+        main.desktopnotification.State,
+        main.desktopnotification.State
     ) -> Unit,
 ) {
     @Synchronized
@@ -24,29 +25,21 @@ class State(
         notificationList = stateUpdater(notificationList)
         val nextState = notificationList::class.java
         System.err.println("$prevState -> $nextState")
-        sendViewModel(notificationList, filter)
+        onChange(notificationList, filter)
     }
 
     @Synchronized
     @JvmName("updateFilter")
     fun update(stateUpdater: StateUpdater<main.filter.State>) {
         filter = stateUpdater(filter)
-        sendViewModel(notificationList, filter)
+        onChange(notificationList, filter)
     }
 
     @Synchronized
     @JvmName("updateDesktopNotification")
     fun update(stateUpdater: StateUpdater<main.desktopnotification.State>) {
         val newState = stateUpdater(desktopNotification)
-        val now = OffsetDateTime.now(ZoneOffset.UTC)
-        val toSend = desktopNotification.holders.flatMap {
-            val gatewayId = it.key
-            val after = newState.holders[gatewayId]!!
-            val before = it.value
-            after.fetched - before.fetched
-        }.filter { it.timestamp > now.minusMinutes(5) }
-        sendDesktopNotification(toSend)
-
+        onChangeDesktop(newState, desktopNotification)
         desktopNotification = newState
     }
 }
